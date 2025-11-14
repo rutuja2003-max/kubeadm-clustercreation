e210e88c6dc361d26d786fed10ebdddc4a1be3b0
rutuja2003-max <143155585+rutuja2003-max@users.noreply.github.com>
2025-11-14 11:10:02 +0530

    Update On-Master.sh

--- On-Master.sh
+++ On-Master.sh
@@ -10,5 +10,6 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+
 #deploy a CNI plugin (like Calico):
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
